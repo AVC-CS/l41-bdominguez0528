a1c03d62baa553361469278e55a9cdb44d06f3fa
Bryant Dominguez <bdominguez0528@gmail.com>
2025-04-11 20:38:13 -0700

    modified main.py

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ def main():
 
     N = int(input('Enter the number N: '))
     result = []
-
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    
+    for i in range(N + 1):
+        print(2 ** i)
+        result.append(2 ** i)
 
     ########################################
     # Do not delete the return statement
